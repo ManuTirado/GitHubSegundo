467cfc7a52bfaeb5c748211fbe596ae84e46d797
Manuel <manuel.tirado@iesnervion.es>
2022-09-26 13:59:33 +0200

    Estándar

---
+++ DesarrolloInterfaces/03-Hello Worlds/03-Hello World-Entidades Estrandar/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace _03_Hello_World_Entidades_Estrandar
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ DesarrolloInterfaces/03-Hello Worlds/03-Hello World-Entidades/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace _03_Hello_World_Entidades
+{
+    public class cls_Persona
+    {
+
+    }
+}
