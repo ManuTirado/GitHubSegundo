a7815aae0f966b54fc4893ec7895278a871bd433
Manuel <manuel.tirado@iesnervion.es>
2022-09-27 14:38:37 +0200

    Ej2 terminao

--- Servicios y Procesos/Ejercicios U1/src/Ej2/ProcesoLento.java
+++ Servicios y Procesos/Ejercicios U1/src/Ej2/ProcesoLento.java
@@ -3,8 +3,8 @@ package Ej2;
 public class ProcesoLento {
 
 	public static void main(String[] args) {
-		for (int i=1; i<=10; i++) {
-			System.out.println(i);
+		for (int i=0; i<10; i++) {
+			System.out.println(i+1);
 			
 			try {
 				Thread.sleep(1000);
