3db7f70c3dc2bc6325ddeb8798b20ff083681f47
Manuel <manuel.tirado@iesnervion.es>
2022-09-27 14:36:26 +0200

    Ej2 terminao

--- Servicios y Procesos/Ejercicios U1/src/Ej2/Main.java
+++ Servicios y Procesos/Ejercicios U1/src/Ej2/Main.java
@@ -1,7 +1,23 @@
 package Ej2;
 
+import java.io.IOException;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ProcessBuilder processBuilder = new ProcessBuilder(args);
+        processBuilder.inheritIO();
+        try {
+            Process process = processBuilder.start();
+
+            while (process.isAlive()) {
+                System.out.println("¡¡ Está vivooooo !!");
+                Thread.sleep(3000);
+            }
+            System.out.println("C murió ;(");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
---
+++ Servicios y Procesos/Ejercicios U1/src/Ej2/ProcesoLento.java
@@ -0,0 +1,18 @@
+package Ej2;
+
+public class ProcesoLento {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=10; i++) {
+			System.out.println(i);
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
