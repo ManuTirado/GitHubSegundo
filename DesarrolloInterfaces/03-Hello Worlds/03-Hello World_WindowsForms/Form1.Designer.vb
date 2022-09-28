c8b38b79c210f1c0cff5542e311c96d11883bf0a
Manuel <manuel.tirado@iesnervion.es>
2022-09-28 10:10:08 +0200

    Terminao

--- DesarrolloInterfaces/03-Hello Worlds/03-Hello World_WindowsForms/Form1.Designer.vb
+++ DesarrolloInterfaces/03-Hello Worlds/03-Hello World_WindowsForms/Form1.Designer.vb
@@ -22,29 +22,52 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnSaludar = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnSaludar
         '
-        Me.Button1.Location = New System.Drawing.Point(326, 211)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnSaludar.Location = New System.Drawing.Point(349, 174)
+        Me.btnSaludar.Name = "btnSaludar"
+        Me.btnSaludar.Size = New System.Drawing.Size(75, 23)
+        Me.btnSaludar.TabIndex = 0
+        Me.btnSaludar.Text = "Saludar"
+        Me.btnSaludar.UseVisualStyleBackColor = True
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(337, 129)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 23)
+        Me.txtNombre.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(280, 132)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Nombre"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.btnSaludar)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnSaludar As Button
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- DesarrolloInterfaces/03-Hello Worlds/03-Hello World_WindowsForms/Form1.vb
+++ DesarrolloInterfaces/03-Hello Worlds/03-Hello World_WindowsForms/Form1.vb
@@ -1,5 +1,11 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+﻿Imports _03_Hello_World_Entidades_Estrandar
 
+Public Class Form1
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSaludar.Click
+        Dim persona As New clsPersona
+        With persona
+            .Nombre = txtNombre.Text
+        End With
+        MessageBox.Show("Hola " + persona.Nombre)
     End Sub
 End Class
