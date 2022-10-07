15401f3b938828b6249c229cac4e6358561acc3b
Manuel <manuel.tirado@iesnervion.es>
2022-10-07 09:54:31 +0200

    EJ1.2c

--- AccesoDatos/Ej 2.2/src/SAX/ComprasHandler.java
+++ AccesoDatos/Ej 2.2/src/SAX/ComprasHandler.java
@@ -14,7 +14,7 @@ public class ComprasHandler extends DefaultHandler {
 
     private ArrayList<Compra> compras = new ArrayList<>();
     private ArrayList<Producto> productosActuales = new ArrayList<>();
-    private Producto productoActual = new Producto();
+    //private Producto productoActual = new Producto();
 
     @Override
     public void startElement(String uri, String localName, String qName,
---
+++ AccesoDatos/EjerClase_07-10-22/src/Main.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Main {
+
+    private static final String RUTA_ENTRADA = "src\\input.txt";
+    private static final String RUTA_SALIDA = "src\\output.txt";
+
+    public static void main(String[] args) {
+        RandomAccessFile rafEntrada;
+        RandomAccessFile rafSalida;
+        String letra;
+
+        try {
+            rafEntrada = new RandomAccessFile(new File(RUTA_ENTRADA), "r");
+            rafSalida = new RandomAccessFile(new File(RUTA_SALIDA), "rw");
+
+            rafEntrada.seek(0);
+            letra = rafEntrada.readLine();
+            while (letra != null) {
+                escribirCaracter(rafSalida, letra);
+
+                letra = rafEntrada.readLine();
+            }
+
+            rafEntrada.close();
+            rafSalida.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void escribirCaracter(RandomAccessFile raf, String letra) {
+        int pos = 0;
+        String letraParaMover;
+        try {
+            raf.seek(pos);
+            if (raf.readLine() != null) {
+                for (int i = (int) raf.length(); i >= 2; i -= 2) {
+                    raf.seek(i - 2);
+                    letraParaMover = raf.readLine();
+                    raf.seek(i);
+                    raf.write(letraParaMover.getBytes());
+                    raf.write("\n".getBytes());
+                }
+                raf.seek(0);
+            }
+            raf.write(letra.getBytes());
+            raf.write("\n".getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    private static char leerCaracter() {
+//        for (int i = 0; i < 2; i++) {
+//
+//        }
+//    }
+}
