82c402dd0bf6c3d93e77b79c2c960c92a33fc736
Manuel <manuel.tirado@iesnervion.es>
2022-10-13 14:40:11 +0200

    13/10/22

---
+++ SistemasGestionEmpresarial/06-HolaMundo-ASP-MVC/06-HolaMundo-ASP-MVC/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using _06_HolaMundo_ASP_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _06_HolaMundo_ASP_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
---
+++ SistemasGestionEmpresarial/06-HolaMundo-ASP-MVC/06-HolaMundo-ASP-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _06_HolaMundo_ASP_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
