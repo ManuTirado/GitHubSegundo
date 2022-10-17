18abf684126146ae9f80f17b84f72103c0e8de34
Manuel <manuel.tirado@iesnervion.es>
2022-10-17 22:34:58 +0200

    Ej1-U7-SGEMP

---
+++ SistemasGestionEmpresarial/07-ASP.NET-MVC/07-ASP.NET-MVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using _07_ASP.NET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _07_ASP.NET_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        
+        public ActionResult Index()
+        {
+            clsPersona persona = new clsPersona();
+            persona.Nombre = "Manuel";
+            persona.Apellido = "Tirado";
+
+            int hora = DateTime.Now.Hour;
+            String saludo;
+            if (hora >= 6 && hora < 12)
+            {
+                saludo = "Buenos días";
+            }
+            else if (hora >= 12 && hora < 20)
+            {
+                saludo = "Buenas tardes";
+            }
+            else
+            {
+                saludo = "Buenas noches";
+            }
+            ViewData["Saludo"] = saludo;
+
+            ViewBag.fecha = DateTime.Now.ToLongDateString();
+
+            return View(persona);
+        }
+
+    }
+}
---
+++ SistemasGestionEmpresarial/07-ASP.NET-MVC/07-ASP.NET-MVC/Models/clsPersona.cs
@@ -0,0 +1,8 @@
+namespace _07_ASP.NET_MVC.Models
+{
+    public class clsPersona
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+    }
+}
