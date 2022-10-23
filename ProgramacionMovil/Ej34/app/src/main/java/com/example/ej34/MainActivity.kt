0dfad70048b50caf931825077dc754e711dea0c6
Manuel <manuel.tirado@iesnervion.es>
2022-10-23 21:06:01 +0200

    a

--- ProgramacionMovil/Ej34/app/src/main/java/com/example/ej34/MainActivity.kt
+++ ProgramacionMovil/Ej34/app/src/main/java/com/example/ej34/MainActivity.kt
@@ -6,24 +6,18 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), comunicador {
-    private val TAG = "MyActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.v(TAG, "Main Creado")
     }
 
     override fun onClickButtonRestar() {
         val valor = this.findViewById<TextView>(R.id.txtAzul)
-        Log.v(TAG,"onClickButtonRestar(): Valor antes: " + valor.text)
-        valor.setText("1")
-        Log.v(TAG,"onClickButtonRestar(): Valor despues: " + valor.text)
+        valor.setText((valor.text.toString().toInt()-1).toString())
     }
 
     override fun onClickButtonSumar() {
         val valor = this.findViewById<TextView>(R.id.txtAzul)
-        Log.v(TAG,"onClickButtonSumar(): Valor antes: " + valor.toString())
-        valor.setText("2")
-        Log.v(TAG,"onClickButtonSumar(): Valor despues: " + valor.text)
+        valor.setText((valor.text.toString().toInt()+1).toString())
     }
 }
