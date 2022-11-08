faa1bde0d4d79191be9f31f1019803daf9287cd1
Manuel <manuel.tirado@iesnervion.es>
2022-11-08 01:47:31 +0100

    aaaaaaa

--- DI/Solares/Solares/Pages/DetelleCita.xaml.cs
+++ DI/Solares/Solares/Pages/DetelleCita.xaml.cs
@@ -4,10 +4,35 @@ namespace Solares.Pages;
 
 public partial class DetelleCita : ContentPage
 {
+    private clsCita detalleCita;
 	public DetelleCita(clsCita cita)
 	{
 		InitializeComponent();
-		lblDetalleCita.Text = cita.NombreCliente;
+        detalleCita = cita;
+        lblTitleView.Text = detalleCita.NombreCliente;
+    }
+
+	public void onImgBtnGaleria (object sender, EventArgs e)
+	{
+
+	}
+
+    public void onImgBtnApta(object sender, EventArgs e)
+    {
+        if (detalleCita.isApta)
+        {
+            detalleCita.isApta = false;
+            ImgBtnApta.BackgroundColor = Colors.IndianRed;
+        }
+        else
+        {
+            detalleCita.isApta = true;
+            ImgBtnApta.BackgroundColor = Colors.ForestGreen;
+        }
+    }
+
+    public void onImgBtnGuardar(object sender, EventArgs e)
+    {
 
     }
 }
